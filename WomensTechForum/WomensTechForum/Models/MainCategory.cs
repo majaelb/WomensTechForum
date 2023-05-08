9cfa4c7fe13a138ac913521e074ca26d4e4d996c
maryyj <maria.holster@outlook.com>
2023-05-08 15:26:01 +0200

    add models

---
+++ WomensTechForum/WomensTechForum/Models/MainCategory.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WomensTechForum.Models;
+
+public class MainCategory
+{
+    public int Id { get; set; }
+    [Required]
+    [Display(Name="Huvudkategori")]
+    public string Name { get; set; }
+}
---
+++ WomensTechForum/WomensTechForum/Models/SubCategory.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WomensTechForum.Models;
+
+public class SubCategory
+{
+    public int Id { get; set; }
+    [Required]
+    [Display(Name = "Subkategori")]
+    public string Name { get; set; }
+    [Display(Name = "Beskrivning")]
+    public string? Description { get; set; }
+    public virtual MainCategory? MainCategory { get; set; }
+    public int MainCategoryId { get; set; }
+}
